a70fbb621f054f857c7607b4c977565f57f6ed7d
Maia <matteusmaia889@gmail.com>
2024-07-22 19:52:55 -0300

    update: plugins file

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "local",
+    providerOptions: {
+      path: "public/uploads", // Diretório onde os arquivos serão armazenados
+      sizeLimit: 1000000, // Limite de tamanho do arquivo em bytes (ajuste conforme necessário)
+    },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
+    },
+  },
+});
